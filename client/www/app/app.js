8a632dc86be4aed70786b2c07b16a475259b6418
ericman93 <ericfeldman93@gmail.com>
2017-01-13 21:49:50 +0200

    fix padding, add lights comonent

--- client/www/app/app.js
+++ client/www/app/app.js
@@ -5,47 +5,58 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('harvis.homepage', []);
+angular.module('harvis.lights', []);
 angular.module('harvis.menu', []);
 angular.module('harvis.core', ['harvis.menu']);
-angular.module('harvis', ['ionic', 'harvis.homepage', 'harvis.core'])
+angular.module('harvis', ['ionic', 'harvis.homepage', 'harvis.lights', 'harvis.core'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'app/core/menu/menu.html',
-    controller: 'MenuCtrl'
-  })
-
-    .state('app.homepage', {
-      url: '/homepage',
-      views: {
-        'menuContent': {
-          templateUrl: 'app/homepage/homepage.html',
-          controller: 'HomepageCtrl'
-        }
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
     });
+  })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/homepage');
-})
-.constant('SERVER_URL', '127.0.0.1:5000');
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'app/core/menu/menu.html',
+        controller: 'MenuCtrl'
+      })
+
+      .state('app.homepage', {
+        url: '/homepage',
+        views: {
+          'menuContent': {
+            templateUrl: 'app/homepage/homepage.html',
+            controller: 'HomepageCtrl'
+          }
+        }
+      })
+
+      .state('app.lights', {
+        url: '/lights',
+        views: {
+          'menuContent': {
+            templateUrl: 'app/lights/lights.html',
+            controller: 'LightsCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/homepage');
+  })
+  .constant('SERVER_URL', '127.0.0.1:5000');
