c4e1f2963831caa0990b1b578b68a97ef5fc83af
Eric Feldman <ericfeldman93@gmail.com>
2017-02-11 15:33:51 +0200

    save host in local sotrage

--- client/www/app/core/services/core.service.js
+++ client/www/app/core/services/core.service.js
@@ -1,7 +1,15 @@
 angular.module('harvis.core')
-  .factory('Core', ['$q', '$http', 'SERVER_URL',
-    function ($q, $http, SERVER_URL) {
-      var host = SERVER_URL;
+  .factory('Core', ['$q', '$http', '$window', 'SERVER_URL',
+    function ($q, $http, $window, SERVER_URL) {
+      function loadHost(){
+        var host = $window.localStorage.getItem('host')
+
+        if (host == null){
+          host = SERVER_URL
+        }
+
+        return host
+      }
 
       function stopOutputs(mode) {
         var deferred = $q.defer();
@@ -16,6 +24,7 @@ angular.module('harvis.core')
         return deferred.promise;
       }
 
+      var host = loadHost();
       var service = {
         stopOutputs: stopOutputs,
         host: host
--- client/www/app/settings/settings.controller.js
+++ client/www/app/settings/settings.controller.js
@@ -1,8 +1,11 @@
 angular.module('harvis.settings')
-  .controller('SettingsCtrl', ['$scope', 'Core',
-    function($scope, Core){
+  .controller('SettingsCtrl', ['$scope', '$window', 'Core',
+    function($scope, $window, Core){
     	$scope.save = function(){
-    		Core.host = $scope.server.host + ":" + $scope.server.port
+    		var host = $scope.server.host + ":" + $scope.server.port
+    		
+    		Core.host = host
+    		$window.localStorage.setItem('host', host)
     	}
 
 		function init(){
