67a2fa2bb4d501bcee168b7e104fa8ac46962309
Eric Feldman <ericfeldman93@gmail.com>
2017-02-20 01:35:32 +0200

    remove commnets

--- server/output/lights.py
+++ server/output/lights.py
@@ -1,25 +1,18 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-# pin = 18 # The pin ID, edit here to change it
-# def setup():
-#     GPIO.setmode(GPIO.BCM)
-#     GPIO.setup(pin, GPIO.OUT)
-#     GPIO.setwarnings(False)
-
-# def on():
-# 	__setPin(True)
-
-# def off():
-# 	__setPin(False)
-
-# def __setPin(mode): # A little redundant function but useful if you want to add logging
-#     GPIO.output(pin, mode)
-
-# setup()
-
+pin = 18 # The pin ID, edit here to change it
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.setwarnings(False)
 
 def on():
-	pass
+	__setPin(True)
 
 def off():
-	pass
+	__setPin(False)
+
+def __setPin(mode): # A little redundant function but useful if you want to add logging
+    GPIO.output(pin, mode)
+
+setup()
