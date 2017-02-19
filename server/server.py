cda44db8c3e70831d2312f22ec45aafc1e54f0ba
Eric Feldman <ericfeldman93@gmail.com>
2017-02-20 01:20:53 +0200

    forgot to add server

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from routes.weather_api import weather_api
 from routes.facts_api import fact_api
 from routes.alarm_api import alarm_api
 from routes.modes_api import mode_api
+from routes.lights_api import lights_api
 from routes.core_api import core_api
 
 def create_app():
@@ -13,6 +14,7 @@ def create_app():
     app.register_blueprint(fact_api, url_prefix='/fact')
     app.register_blueprint(alarm_api, url_prefix='/alarm')
     app.register_blueprint(mode_api, url_prefix='/mode')
+    app.register_blueprint(lights_api, url_prefix='/lights')
     app.register_blueprint(core_api)
     
     CORS(app)
