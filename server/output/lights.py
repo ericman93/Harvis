95e9a33f585f1dcc2768d0f952fff0e370e87455
Eric Feldman <ericfeldman93@gmail.com>
2017-02-20 01:18:09 +0200

    lights api

--- server/output/lights.py
+++ server/output/lights.py
@@ -1,18 +1,25 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
-pin = 18 # The pin ID, edit here to change it
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.setwarnings(False)
+# pin = 18 # The pin ID, edit here to change it
+# def setup():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setup(pin, GPIO.OUT)
+#     GPIO.setwarnings(False)
+
+# def on():
+# 	__setPin(True)
+
+# def off():
+# 	__setPin(False)
+
+# def __setPin(mode): # A little redundant function but useful if you want to add logging
+#     GPIO.output(pin, mode)
+
+# setup()
+
 
 def on():
-	__setPin(True)
+	pass
 
 def off():
-	__setPin(False)
-
-def __setPin(mode): # A little redundant function but useful if you want to add logging
-    GPIO.output(pin, mode)
-
-setup()
+	pass
--- server/routes/lights_api.py
+++ server/routes/lights_api.py
@@ -1,15 +1,23 @@
 from flask import Blueprint, jsonify, request
-from output.lights import change_color
+import output.lights as lights
 
-light_api = Blueprint('light_api', 'light_api')
+lights_api = Blueprint('lights_api', 'lights_api')
 
-@fact_api.route('/<color>', methods=['POST'])
-def get_fun_fact(hex_color):
-    red = int(hex_color[0:2])
-    green = int(hex_color[2:4])
-    blue = int(hex_color[4:6])
-    rgb = (red, green, blue)
+@lights_api.route('/', methods=['POST'])
+def lights_on():
+    lights.on()
     
+    return jsonify({})
+
+@lights_api.route('/', methods=['DELETE'])
+def lights_off():
+    lights.off()
+    
+    return jsonify({})
+
+
+@lights_api.route('/', methods=['GET'])
+def light_status():
     return jsonify({
-        'rgc': str(rgb)
-    })
+    	'status': True	
+	})
